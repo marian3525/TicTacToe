bf1a4f8d2983f0b1fc19d678ae80d3fc464206a8
marian3525 <marian3525@yahoo.com>
2015-11-18 17:56:31 +0200

    Commit no. 2

--- app/src/main/java/debug/marian/com/tictactoe/SplashScreenActivity.java
+++ app/src/main/java/debug/marian/com/tictactoe/SplashScreenActivity.java
@@ -1,11 +1,14 @@
 package debug.marian.com.tictactoe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -14,6 +17,21 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        final int SPLASH_SCREEN_TIMEOUT = 5000;
+
+        final Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+        Runnable startMain = new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+            }
+        };
+
+        Handler handler = new Handler();
+        handler.postDelayed(startMain, SPLASH_SCREEN_TIMEOUT);
+
+        super.onDestroy();
     }
 
 }
