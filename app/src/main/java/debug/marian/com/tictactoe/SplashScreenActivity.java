e1b9290b15ded462cda67478476a5234ff956a4d
marian3525 <marian3525@yahoo.com>
2015-11-26 21:59:12 +0200

    Updated splashScreen

--- app/src/main/java/debug/marian/com/tictactoe/SplashScreenActivity.java
+++ app/src/main/java/debug/marian/com/tictactoe/SplashScreenActivity.java
@@ -1,21 +1,12 @@
 package debug.marian.com.tictactoe;
 
 import android.content.Intent;
-import android.graphics.Point;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
-import android.util.Log;
-import android.view.Display;
-import android.view.View;
-
-import android.view.ViewGroup;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.view.animation.AnimationSet;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
+
 
 
 public class SplashScreenActivity extends AppCompatActivity {
